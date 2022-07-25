3e4ea1b01c9710fc0af55e9554a6166960b0f72b
Sai Krishna Bhopathi <90410305+SaikrishnaBhopathi2086040@users.noreply.github.com>
2022-07-25 23:38:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import unittest
 from main import app
+import datetime
+import random
 
 
 class MyTestCase(unittest.TestCase):
@@ -8,21 +10,33 @@ class MyTestCase(unittest.TestCase):
         response = tester.get("/")
         self.assertTrue('200' in str(response))
 
+
     def test_registrationpage_succeeds(self):
+        tester = app.test_client(self)
+        num = random.randint(100,999)
+        response = tester.post("/register", data=dict(
+            username="admin"+str(num),
+            password="password"
+        ))
+        self.assertTrue('302' in str(response))
+
+
+
+    def test_profileget_succeeds(self):
         tester = app.test_client(self)
         response = tester.post("/", data=dict(
             username="admin",
             password="password"
         ))
-        self.assertTrue('302' in str(response))
-
-    def test_profileget_succeeds(self):
-        tester = app.test_client(self)
         response = tester.get("/profile")
         self.assertTrue('200' in str(response))
 
     def test_profilepost_succeeds(self):
         tester = app.test_client(self)
+        response = tester.post("/", data=dict(
+            username="admin",
+            password="password"
+        ))
         response = tester.post("/profile", data=dict(
             fullname="fgsdfgsfgsgsdgrere",
             address1="sfsafskk",
@@ -34,15 +48,41 @@ class MyTestCase(unittest.TestCase):
         ))
         self.assertTrue('302' in str(response))
 
+    def test_quoteget_succeeds(self):
+        tester = app.test_client(self)
+        response = tester.post("/", data=dict(
+            username="admin",
+            password="password"
+        ))
+        response = tester.post("/quote", data=dict(
+            quantity=12,
+            DeliveryDate = datetime.datetime.now(),
+            SuggestedPrice = 4.0,
+            TotalAmount = 10.0
+        ))
+        response = tester.get("/quote")
+        self.assertTrue('200' in str(response))
+
     def test_quotepost_succeeds(self):
         tester = app.test_client(self)
+        response = tester.post("/", data=dict(
+            username="admin",
+            password="password"
+        ))
         response = tester.post("/quote", data=dict(
-            quantity=12
+            quantity=12,
+            DeliveryDate = datetime.datetime.now(),
+            SuggestedPrice = 4.0,
+            TotalAmount = 10.0
         ))
         self.assertTrue('302' in str(response))
 
     def test_histroyget_succeeds(self):
         tester = app.test_client(self)
+        response = tester.post("/", data=dict(
+            username="admin",
+            password="password"
+        ))
         response = tester.get("/history")
         self.assertTrue('200' in str(response))
 
